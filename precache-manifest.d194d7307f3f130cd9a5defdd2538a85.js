b9526d66d2277fbfceb33c9ac9cce6923ac3c5d6
HOJUN <wns674@naver.com>
2020-07-18 00:44:09 +0900

    Updates

---
+++ precache-manifest.d194d7307f3f130cd9a5defdd2538a85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c458b74c6c31df072561352facd2fc",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d9cd6dfec6bca1f1795d",
+    "url": "/movie_app/static/js/2.d710c1aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.d710c1aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c907d22cd39b08a58b",
+    "url": "/movie_app/static/js/main.f90f9c76.chunk.js"
+  },
+  {
+    "revision": "b9cb9a6795ceb34c84cd",
+    "url": "/movie_app/static/js/runtime-main.f01f2091.js"
+  }
+]);
